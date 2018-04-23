058e14b7b508cd957ec3b44e78ca23c04962b53e
zachchao <zach.chao@yahoo.com>
2018-04-23 11:00:39 -0700

    Problem 112

---
+++ Problem112.py
@@ -0,0 +1,32 @@
+
+def increasing(n):
+	last = -1
+	for current in str(n):
+		if int(current) < last:
+			return False
+		last = int(current) 
+	return True
+
+def decreasing(n):
+	last = 10
+	for current in str(n):
+		if int(current) > last:
+			return False
+		last = int(current) 
+	return True
+
+
+notBouncy = 0
+bouncy = 0
+
+i = 1
+while True:
+	if not increasing(i) and not decreasing(i):
+		bouncy += 1
+	else:
+		notBouncy += 1
+	if int((bouncy / (notBouncy + bouncy)) * 100) == 99:
+		print(i)
+		break
+	else:
+		i += 1
